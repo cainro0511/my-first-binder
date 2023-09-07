68f0c694138dabde8d2bf3f073c0b57d71cd4fe1
cainro0511 <144364734+cainro0511@users.noreply.github.com>
2023-09-07 11:23:52 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello from Binder!")
+echo "# my-first-binder" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cainro0511/my-first-binder.git
+git push -u origin main
